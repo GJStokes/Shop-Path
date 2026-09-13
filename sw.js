db7f487c376e4a9c90dba8f25ffdc08a7dd1fce1
GJStokes <gjstokes3@gmail.com>
2026-09-13 18:42:39 +1200

    Update service worker cache

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='grocery-route-v1';
+const CACHE='shop-path-v2';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
